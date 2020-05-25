cd431fa25d2f4fe2ba52f74869696d0ae2d9d071
albingroen <albin.groen@gmail.com>
2020-05-25 19:56:22 +0200

    🔥 Add linting and formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 /** @jsx jsx */
-import { jsx, Heading } from "theme-ui"
+import { jsx, Heading } from 'theme-ui'
 
 const Home = () => {
   return (
-    <header sx={{ backgroundColor: "primary", display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <header
+      sx={{
+        backgroundColor: 'primary',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
       <Heading sx={{ color: 'background', fontSize: 7 }}>ui-diff</Heading>
     </header>
   )
